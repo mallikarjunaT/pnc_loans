ad3ab9de1e9fd5aa2b973bfc91226b276cf61326
mallikarjuna <mallikarjuna@hirextra.in>
2025-03-23 19:41:18 +0530

    address dynamic added

--- src/app/reactive-forms/reactive-forms.component.ts
+++ src/app/reactive-forms/reactive-forms.component.ts
@@ -17,7 +17,9 @@ export class ReactiveFormsComponent implements OnInit {
       lname: new FormControl('', [Validators.required, Validators.minLength(1), Validators.maxLength(15), Validators.pattern('^[a-zA-Z]*$')]),
       contact: new FormControl('', [Validators.required, Validators.maxLength(10), Validators.pattern('^[6-9][0-9]{9}$')]),
       state: new FormControl('', [Validators.required]),
-      zipcode: new FormControl('', [Validators.required, Validators.maxLength(6),Validators.pattern('[1-9]{1}[0-9]{5}|[1-9]{1}[0-9]{3}\\s[0-9]{3')]),
+      zipcode: new FormControl('', [Validators.required, Validators.maxLength(6), Validators.pattern('[1-9]{1}[0-9]{5}|[1-9]{1}[0-9]{3}\\s[0-9]{3')]),
+      address: new FormArray([
+      ])
     })
 
   }
@@ -26,6 +28,22 @@ export class ReactiveFormsComponent implements OnInit {
   }
   get mfm(): { [key: string]: AbstractControl } {
     return this.myform.controls;
+  }
+  get address(): FormArray {
+    return this.myform.get('address') as FormArray;
+  }
+
+  addAddress() {
+    const add = this.fb.group({
+      street: new FormControl('', Validators.required),
+      village: new FormControl('', Validators.required),
+      mandal: new FormControl('', Validators.required),
+      dist: new FormControl('', Validators.required)
+    })
+    this.address.push(add);
+  }
+  remove($index: number) {
+    this.address.removeAt($index)
   }
   submitform() {
     console.log(this.myform.value);
